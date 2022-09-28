679d9f8d78f8c42c9ca5aca9441dce4651357c42
Daniel-Santin <32037139@mackenzista.com.br>
2022-09-28 19:42:52 -0300

    comitt3

--- JS/script.js
+++ JS/script.js
@@ -3,13 +3,15 @@ const todoInput = document.querySelector("#textInput");
 const todolist = document.querySelector("#lista");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#editInput");
-const cancelEdit = document.querySelector(".Btcancel");
+const cancelEdit = document.querySelector("#remove_task");
+
+let oldValue;
 
 //funções
 const saveTodo = (text) =>{
 
-    const todo = document.querySelector("div");
-    todo.classList.add("to-do");
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
 
     const todoTitle = document.createElement("p");
     todoTitle.innerHTML = text;
@@ -33,7 +35,27 @@ const saveTodo = (text) =>{
     todolist.appendChild(todo);
 
     todoInput.value = "";
+    todoInput.focus();
+
 };
+
+const toggleForms = () =>{
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todolist.classList.toggle("hide");
+}
+
+const updateTodo = (text) => {
+    const alltodo = document.querySelectorAll("todo");
+
+    alltodo.forEach((todo) => {
+        let todoTitle = todo.querySelector("p");
+
+        if(todoTitle.innerText ===  oldValue){
+            todoTitle.innerText = text;
+        }
+    })
+}
 //Events
 
 todoForm.addEventListener("submit",(e)=>{
@@ -44,4 +66,51 @@ todoForm.addEventListener("submit",(e)=>{
     if (inputValue){
         saveTodo(inputValue);
     }
+});
+
+document.addEventListener("click", (e) => {
+
+    const BtClick = e.target;
+    const AcessTask = BtClick.closest("div");
+    let todoTitle;
+
+    
+    if(AcessTask && AcessTask.querySelector("p")){
+        todoTitle = AcessTask.querySelector("p").innerText;
+
+    }
+
+    if(BtClick.classList.contains("done_task")){
+        AcessTask.classList.toggle("done");
+    }
+
+    if(BtClick.classList.contains("remove_task")){
+        e.preventDefault();
+       AcessTask.remove();
+    }
+
+    if(BtClick.classList.contains("edit_task")){
+        e.preventDefault();
+        toggleForms();
+
+        editInput.value = todoTitle;
+
+     }
+
+});
+
+cancelEdit.addEventListener("click", (e) =>{
+ e.preventDefault();
+
+ toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const editInputValue = editInput.value;
+    if(editInputValue){
+        updateTodo(editInputValue);
+    }
+    toggleForms();
 });
